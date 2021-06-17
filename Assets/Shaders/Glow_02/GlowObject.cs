cc684ac4c89a1844fea7463f9a12deea04c19003
SHaus23 <samuel.hauser@carrier.com>
2021-06-17 13:12:44 -0400

    Glow effect

--- Assets/Shaders/Glow_02/GlowObject.cs
+++ Assets/Shaders/Glow_02/GlowObject.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class GlowObject : MonoBehaviour
 {
     public Material newMaterial;
-    public float val = 1;
-    float lerp = 0f, duration = 2.0f;
+    public float val;
+    float lerp = 0f, duration = .5f;//duration is the length of the effect
+    int pulseAmounts = 3;
     
     // Start is called before the first frame update
 
@@ -18,24 +19,73 @@ public class GlowObject : MonoBehaviour
 
     public void Changematerial(float amount)
     {
-        //Call this function from a button
-        //newMaterial.SetFloat("GlowAmount_01234", amount);      
-        lerp += Time.deltaTime / duration;
-        var lerpAmt = Mathf.Lerp(val, amount, lerp);
-        newMaterial.SetFloat("GlowAmount_01234", lerpAmt);
-        StartCoroutine(ResetMaterial(amount));
+        /* //Call this function from a button
+         //newMaterial.SetFloat("GlowAmount_01234", amount);      
+         lerp += Time.deltaTime / duration;
+         var lerpAmt = Mathf.Lerp(val, amount, lerp);
+         newMaterial.SetFloat("GlowAmount_01234", lerpAmt);
+         StartCoroutine(ResetMaterial(amount));
+        */
+        newMaterial.SetFloat("GlowAmount_01234", val);
+        
+            lerp = 0;
+            StartCoroutine(GlowEffect(amount));
+ 
     }
-
-    public IEnumerator ResetMaterial (float amount)
+  
+    public IEnumerator GlowEffect(float amount)
     {
-        yield return new WaitForSeconds(2.0f);
+        for (int i = 0; i < pulseAmounts; i++)
+        {
+            lerp = 0;//reset lerp every itteration so that glowvalue resets
+            float timePassed = 0;
+            newMaterial.SetFloat("GlowAmount_01234", val);
+            while (timePassed <= duration)//glow increases
+            {
+                lerp += Time.deltaTime / duration;
+                var lerpAmt = Mathf.Lerp(val, -amount, lerp);//negitive sign on amount accouts for negitive glow values make
+                //the material glow
+                newMaterial.SetFloat("GlowAmount_01234", lerpAmt);
+                yield return null;
+                timePassed += Time.deltaTime;
+
+            }
+           
+            yield return new WaitForSeconds(.5f);//pause with the max glow
+            lerp = 0;
+
+            //put inside a while loop
+            timePassed = 0;//reset time passed for effect
+            while (timePassed <= duration)//glow decreses
+            {
+                lerp += Time.deltaTime / duration;
+                var lerpAmt = Mathf.Lerp(-amount, val, lerp);
+                newMaterial.SetFloat("GlowAmount_01234", lerpAmt);
+                yield return null;
+                timePassed += Time.deltaTime;
+            }
+            newMaterial.SetFloat("GlowAmount_01234", val);
+            yield return new WaitForSeconds(.2f);//pause with no glow
+        }
+    }
+    /*public IEnumerator ResetMaterial (float amount)
+    {
+        yield return new WaitForSeconds(1.0f);
         //newMaterial.SetFloat("GlowAmount_01234", val);
-        lerp = 0; 
+        lerp = 0;
 
         //put inside a while loop
-        lerp += Time.deltaTime / duration;
-        var lerpAmt = Mathf.Lerp(amount, val, lerp);
-        newMaterial.SetFloat("GlowAmount_01234", lerpAmt);
+        float timePassed = 0;
+        while (timePassed <= duration)
+        {
+            lerp += Time.deltaTime / duration;
+            var lerpAmt = Mathf.Lerp(-amount, val, lerp);
+            newMaterial.SetFloat("GlowAmount_01234", lerpAmt);
+            Debug.Log("GLow amount fall: " + newMaterial.GetFloat("GlowAmount_01234"));
+            yield return null;
+            timePassed += Time.deltaTime;
+        }
+        newMaterial.SetFloat("GlowAmount_01234", val);
     }
-      
+      */
 }
